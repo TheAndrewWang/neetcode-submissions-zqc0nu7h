e24288c906b9e3b589ae45101fc37b9b061877da
Andrew Wang <andrewwangpa@gmail.com>
2026-08-04 12:46:48 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        if (prices.size() == 1 ) {
+            return 0;
+        }
+
+        int l = 0; int r = 1;
+        while (r < prices.size()) {
+            if (prices[r] > prices[l]) {
+                profit = max(profit, prices[r] - prices[l]);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+
+        return profit;
+    }
+};
